ad52a4ce258a17e3a3732afeb5ecdef302358f44
Juliana Paz <julianapaz07@hotmail.com>
2014-05-06 09:59:09 -0300

    Adicionando classe ArvoreBinaria

---
+++ Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
@@ -0,0 +1,111 @@
+package com.senac.estruturas;
+
+public class ArvoreBinaria<T extends Comparable<T>> 
+{
+	
+	private Nodo raiz;
+	
+	
+	public ArvoreBinaria(Nodo raiz)
+	{
+		this.raiz = raiz;
+
+	}
+	
+	public ArvoreBinaria()
+	{
+		
+	}
+	
+	public Nodo getRaiz()
+	{
+		return raiz;
+	}
+	
+	public Nodo insere(Nodo r, Nodo novo)
+	{
+		if( r==null )
+			r = novo;
+		else
+		{
+			int cmp = novo.compareTo(r);
+			//System.out.println(cmp);
+			
+			if( cmp < 0 ) 
+			{
+				if( r.getEsquerda() == null )
+					r.setEsquerda(novo);
+				else
+					r.setEsquerda(insere(r.getEsquerda(),novo));
+			}
+			else if( cmp > 0 )
+			{
+				if( r.getDireita() == null )
+					r.setDireita(novo);
+				else
+					r.setDireita(insere(r.getDireita(),novo));
+			}
+			else
+				System.out.println("Nodo ja existe na arvore");
+				
+		}
+		
+		
+		return r;
+	}
+
+
+public int compareTo(Nodo o) {
+		return raiz.compareTo(o);
+	}
+	
+	//arrumar
+	public void printInfixa(Nodo nodo)
+	{
+		if( nodo != null )
+		{
+			printInfixa(nodo.getEsquerda());
+			System.out.println(nodo);
+			printInfixa(nodo.getDireita());
+			
+		}
+			
+	}
+	
+	public void printPrefixa(Nodo nodo)
+	{
+		if( nodo != null )
+		{
+			System.out.println(nodo);
+			printInfixa(nodo.getEsquerda());
+			printInfixa(nodo.getDireita());
+		}
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		ArvoreBinaria<String> arvore = new ArvoreBinaria<>(new Nodo("j","716276336"));
+		
+		arvore.insere(arvore.getRaiz(), new Nodo("d","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("b","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("c","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("d","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("e","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("f","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("g","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("a","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("p","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("r","282737347"));
+		arvore.insere(arvore.getRaiz(), new Nodo("k","282737347"));
+		
+		System.out.println("Travessia Infixa");
+		arvore.printInfixa(arvore.getRaiz());
+		System.out.println("\nTravessia Prefixa");
+		System.out.println();
+	}
+	
+}
