7f1edb023dbadac83d2737748d94ed483d6dfc95
Juliana Paz <julianapaz07@hotmail.com>
2014-05-07 15:15:40 -0300

    Remover nodo pela chave

--- Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
+++ Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
@@ -165,10 +165,167 @@ public class ArvoreBinaria<T extends Comparable<T>>
 					
 		}
 	}
+	/**
+	 * Metodo recursivo de remover um nodo
+	 * @param r
+	 * @param chave
+	 * @return
+	 */
 	
 	
+	public Nodo remove(Nodo r, String chave)
+	{
+		
+		if(r == null)
+			System.out.println("Nao existe o dado na arvore");
+		
+		else
+		{
+			int cmp = chave.compareTo(r.getChave());
+			
+			//nao achou e esta na parte direita da arvore
+			//eh maior que a raiz
+			if ( cmp > 0)
+				remove(r.getDireita(), chave);
+			
+			//nao achou e esta na parte esquerda da arvora
+			//eh menor que a raiz
+			else if ( cmp < 0)
+				remove(r.getEsquerda(), chave);
+			
+			//encontrou o nodo
+			else
+			{
+				/*verifica se o nodo a ser removido eh
+				 * folha
+				 * raiz com um filho a direita
+				 * raiz com um filho a esquerda
+				 * raiz com dois filhos
+				 */
+				
+				/*
+				 * verifica se o nodo a ser excluido eh uma folha 
+				 * busca o pai do nodo e apaga a referencia da esq ou da dir
+				 */
+				if ( r.getDireita()==null && r.getEsquerda() == null )
+				{
+					System.out.println("Pai " + buscaPai(r));
+					System.out.println("Eh uma folha!");
+					Nodo pai = buscaPai(r);
+					
+					//se o filho for da direita
+					if ( r.compareTo(pai) > 0 )
+						//apaga a referencia da direita
+						pai.setDireita(null);
+					//senao
+					else
+						//apaga referencia da esquerda
+						pai.setEsquerda(null);
+					
+				}
+				
+				
+				/*
+				 * Verifica se o nodo tem um filho a esquerda
+				 * filho a esquerda passa a ser a raiz
+				 */
+				else if ( r.getDireita() == null)
+				{
+					
+					Nodo pai = buscaPai(r);
+					
+					if ( r.compareTo(pai) > 0 )
+						pai.setDireita(r.getEsquerda());
+					else
+						pai.setEsquerda(r.getEsquerda());
+					r.setEsquerda(null);
+			
+				}
+				
+				/* Verifica se o nodo tem um filho a esquerda
+				 * filho a esquerda passa a ser a raiz 
+				 */
+				
+				else if ( r.getEsquerda() == null)
+				{
+					
+					Nodo pai = buscaPai(r);
+					
+					if ( r.compareTo(pai) > 0 )
+						pai.setDireita(r.getDireita());
+					else
+						pai.setEsquerda(r.getDireita());
+					r.setDireita(null);
+			
+				}
+
+				//nodo tem filhos esq e direita
+				//busca a menor folha da direita da esquerda
+				//a menor folha assume o lugar da raiz - nodo a ser removido
+				// nodo ser removido recebe as informacoes da menor folha
+				// apaga as referencias do antigo pai da folha
+				else
+				{
+					Nodo menorFolha = menorFolhaEsquerda(r.getDireita());
+					Nodo pai = buscaPai(menorFolha);
+					
+					if(menorFolha.compareTo(pai) > 0)
+						pai.setDireita(null);
+					else
+						pai.setEsquerda(null);
+					r.setChave(menorFolha.getChave());
+					r.setDado(menorFolha.getDado());
+				}
+					
+			}
+				
+		}
+		
+		
+		
+		return r;
+	}
 	
 	
+	public Nodo buscaPai(Nodo filho)
+	{
+		
+		//System.out.println("FIlho " + filho);
+		
+		if ( filho == raiz)
+			return null;
+		
+		Nodo anterior = null;
+		Nodo atual = raiz;
+		
+		while( filho != atual)
+		{
+			//System.out.println("Filho "+filho);
+			anterior = atual;
+			//System.out.println("Anterior "+anterior);
+			if( filho.compareTo(atual)>0)
+					atual = atual.getDireita();
+			else
+				atual = atual.getEsquerda();
+		}
+		
+		return anterior;
+		
+	}	
+	
+	public Nodo menorFolhaEsquerda(Nodo nodo)
+	{
+		System.out.println("Metodo menor a direita da esquerda");
+		
+		while(nodo.getEsquerda() != null )
+		{
+			nodo = nodo.getEsquerda();
+			System.out.println("Nodo esquerda " + nodo);
+		}
+		
+		return nodo;
+	}
+		
 	public static void main(String[] args) {
 		
 		ArvoreBinaria<String> arvore = new ArvoreBinaria<>(new Nodo("j","716276336"));
@@ -200,6 +357,9 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		arvore.busca(arvore.getRaiz(), "a", 0);
 		arvore.busca(arvore.getRaiz(), "x", 0);
 
+		
+		arvore.remove(null, "j");
+		arvore.printInfixa(arvore.getRaiz());
 	}
 	
 }
