35df0fb8ccb14cbebe86f911595079adfc129e74
Juliana Paz <julianapaz07@hotmail.com>
2014-05-07 14:46:12 -0300

    Busca de nodo

--- Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
+++ Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
@@ -4,11 +4,13 @@ public class ArvoreBinaria<T extends Comparable<T>>
 {
 	
 	private Nodo raiz;
+
 	
 	
 	public ArvoreBinaria(Nodo raiz)
 	{
 		this.raiz = raiz;
+		
 
 	}
 	
@@ -22,6 +24,9 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		return raiz;
 	}
 	
+	
+	
+	
 	public Nodo insere(Nodo r, Nodo novo)
 	{
 		if( r==null )
@@ -29,12 +34,12 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		else
 		{
 			int cmp = novo.compareTo(r);
-			//System.out.println(cmp);
-			
+		
 			if( cmp < 0 ) 
 			{
 				if( r.getEsquerda() == null )
 					r.setEsquerda(novo);
+		
 				else
 					r.setEsquerda(insere(r.getEsquerda(),novo));
 			}
@@ -54,12 +59,14 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		return r;
 	}
 
-
-public int compareTo(Nodo o) {
+	/*
+	 * Nao sei usar compare
+	 */
+	public int compareTo(Nodo o) {
 		return raiz.compareTo(o);
 	}
 	
-	//arrumar
+	
 	public void printInfixa(Nodo nodo)
 	{
 		if( nodo != null )
@@ -84,122 +91,13 @@ public int compareTo(Nodo o) {
 	
 	
 	
-	/**
-	 * Metodo que busca um contato pela chave
-	 * @param nodo
-	 * @param dado
-	 * @return
-	 */
-	
-	public Nodo busca(Nodo nodo, String dado)
-	{
-		
-		if ( nodo == null)
-			System.out.println("Dado nao existe na arvore");
-		else
-		{
-			//System.out.println("Nodo atual: " + nodo);
-			//System.out.println("Chave: " + nodo.getChave());
-			int cmp = dado.compareTo(nodo.getChave());
-			System.out.println("CMp: "+cmp);
-			
-			if( cmp == 0 )
-				System.out.println(nodo);
-			
-			else if( cmp < 0 )
-				busca(nodo.getEsquerda(), dado);
-			else
-				busca(nodo.getDireita(),dado);
-					
-		}
-		return nodo;
-	}
-	
-	public Nodo remove(Nodo nodo, String chave)
-	{
-		
-		if( nodo == null )
-			System.out.println("O dado nao existe na arvore");
-		else
-		{
-			int cmp = chave.compareTo(nodo.getChave());
-			//System.out.println("Comparacao: " + nodo.getChave() + " com " + chave + ": " + cmp);
-			
-			
-			if ( cmp < 0)
-				remove(nodo.getEsquerda(), chave);
-		
-			else if ( cmp > 0)
-				remove(nodo.getDireita(),chave);
-			
-			
-			
-			else
-			{
-				//se nodo nao tem filhos - folha
-				if(nodo.getEsquerda()== null && nodo.getDireita() == null)
-				{
-					//nao apaga?????
-					nodo = null;
-					System.out.println("Nodo null");
-				}
-				//se nodo tem um filho a direita
-				else if(nodo.getEsquerda() == null)
-				{
-					nodo = nodo.getDireita();
-					System.out.println("Filho a direita vira raiz");
-				}
-				
-				//se nodo tem um filho a esquerda
-				else if( nodo.getDireita() == null )
-				{
-					nodo = nodo.getEsquerda();
-					System.out.println("Filho a esquerda vira raiz");
-				}
-				
-				else
-				{
-					Nodo maiorFolha = maiorFolhaDireita(nodo);
-					System.out.println("Maior filho " + maiorFolha);
-					nodo.setChave(maiorFolha.getChave());
-					nodo.setDado(maiorFolha.getDado());
-					
-					//apaga a maior folha
-					maiorFolha = null;
-					
-				}
-				
-				
-				
-			}
-		}
-		return nodo;
-	}
 	
 	
-	public Nodo maiorFolhaDireita(Nodo nodo)
-	{
-		
-		while(nodo.getDireita() != null )
-			nodo = nodo.getDireita();
-		
-		return nodo;
-	}
-
-
-
-	
-	
-	
-	
-	
-
 	
 	public static void main(String[] args) {
 		
 		ArvoreBinaria<String> arvore = new ArvoreBinaria<>(new Nodo("j","716276336"));
-		
-		arvore.insere(arvore.getRaiz(), new Nodo("d","282737347"));
+
 		arvore.insere(arvore.getRaiz(), new Nodo("b","282737347"));
 		arvore.insere(arvore.getRaiz(), new Nodo("c","282737347"));
 		arvore.insere(arvore.getRaiz(), new Nodo("d","282737347"));
@@ -211,10 +109,7 @@ public int compareTo(Nodo o) {
 		arvore.insere(arvore.getRaiz(), new Nodo("r","282737347"));
 		arvore.insere(arvore.getRaiz(), new Nodo("k","282737347"));
 		
-		System.out.println("Travessia Infixa");
-		arvore.printInfixa(arvore.getRaiz());
-		System.out.println("\nTravessia Prefixa");
-		System.out.println();
+
 	}
 	
 }
