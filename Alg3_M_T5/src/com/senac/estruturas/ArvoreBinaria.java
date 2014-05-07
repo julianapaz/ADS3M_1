3270cc2ccf87dd5e8fcc167f20f417cab88e0940
Juliana Paz <julianapaz07@hotmail.com>
2014-05-07 15:03:44 -0300

    Busca pela chave com nr de comparacoes

--- Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
+++ Alg3_M_T5/src/com/senac/estruturas/ArvoreBinaria.java
@@ -107,7 +107,64 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		}
 	}
 	
+	/**
+	 * Metodo que busca um nodo na arvore
+	 * se existir exibe seu conteudo e nr de comparacoes
+	 * @param nodoRaiz
+	 * @param chave
+	 * @param nrComparacoes
+	 */
 	
+	public void busca(Nodo nodoRaiz, String chave, int nrComparacoes)
+	{
+		/*
+		 * Quando ja percorreu toda arvore
+		 * pegando o nodo raiz da ultima busca
+		 * o qual eh uma referencia da esquerda ou direita
+		 * por nao possuir mais elemento eh nulo 
+		 */
+		if ( nodoRaiz == null)
+			System.out.println("Dado nao existe na arvore" +
+					"\nNumero de comparacoes: " + nrComparacoes);
+		else
+		{
+			//compara a chave de busca com a raiz
+			int cmp = chave.compareTo(nodoRaiz.getChave());
+			
+			/*
+			 * Achou
+			 * Imprime o nodo e o nr de comparoes
+			 */
+			if( cmp == 0 )
+			{
+				nrComparacoes++;
+				System.out.println(nodoRaiz + "\nNumero de comparacoes: " + nrComparacoes);				
+			}				
+			
+			/*
+			 * a chave de busca eh maior que a raiz 
+			 * incrementa o nr de comparacoes
+			 * chama metodo de busca com o filho da esquerda como raiz
+			 */
+			else if( cmp < 0 )
+			{
+				nrComparacoes++;
+				busca(nodoRaiz.getEsquerda(), chave, nrComparacoes);
+					
+			}
+			/*
+			 * a chave de busca eh menor que a raiz 
+			 * incrementa o nr de comparacoes
+			 * chama metodo de busca com o filho da direita como raiz
+			 */
+			else
+			{
+				nrComparacoes++;
+				busca(nodoRaiz.getDireita(),chave, nrComparacoes);
+			}
+					
+		}
+	}
 	
 	
 	
@@ -137,6 +194,11 @@ public class ArvoreBinaria<T extends Comparable<T>>
 		System.out.println(arvore.getTotalNodos());
 		arvore.insere(arvore.getRaiz(), new Nodo("k","282737347"));
 		System.out.println(arvore.getTotalNodos());
+		
+		arvore.busca(arvore.getRaiz(), "j", 0);
+		arvore.busca(arvore.getRaiz(), "g", 0);
+		arvore.busca(arvore.getRaiz(), "a", 0);
+		arvore.busca(arvore.getRaiz(), "x", 0);
 
 	}
 	
